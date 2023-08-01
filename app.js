077cf4f3a4579a5774914ab20665dd0791db22d7
Hennadii Borysevych <genuch3@gmail.com>
2023-08-01 22:26:44 +0200

    feat:changes

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 
-const authRouter = require("./routes/api/auth");
-const usersRouter = require("./routes/api/users");
-const boardsRouter = require("./routes/api/boards");
-const columnsRouter = require("./routes/api/columns");
-const cardsRouter = require("./routes/api/cards");
+// const authRouter = require("./routes/api/auth");
+// const usersRouter = require("./routes/api/users");
+// const boardsRouter = require("./routes/api/boards");
+// const columnsRouter = require("./routes/api/columns");
+// const cardsRouter = require("./routes/api/cards");
 
 require("dotenv").config();
 
@@ -18,11 +18,11 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use("/api/auth", authRouter);
-app.use("/api/users", usersRouter);
-app.use("/api/boards", boardsRouter);
-app.use("/api/columns", columnsRouter);
-app.use("/api/cards", cardsRouter);
+// app.use("/api/auth", authRouter);
+// app.use("/api/users", usersRouter);
+// app.use("/api/boards", boardsRouter);
+// app.use("/api/columns", columnsRouter);
+// app.use("/api/cards", cardsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not Found" });
