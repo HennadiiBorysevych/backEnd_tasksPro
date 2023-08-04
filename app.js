6e871e2042c7dd19e7cbe4e41ab3eab8076a0a4d
Hennadii Borysevych <genuch3@gmail.com>
2023-08-04 15:05:58 +0200

    feat: auth swagger

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
 const authRouter = require("./routes/api/auth");
- const usersRouter = require("./routes/api/users");
+const usersRouter = require("./routes/api/users");
 // const boardsRouter = require("./routes/api/boards");
 // const columnsRouter = require("./routes/api/columns");
 // const cardsRouter = require("./routes/api/cards");
@@ -17,8 +20,10 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use("/api/auth", authRouter);
- app.use("/api/users", usersRouter);
+app.use("/api/users", usersRouter);
 // app.use("/api/boards", boardsRouter);
 // app.use("/api/columns", columnsRouter);
 // app.use("/api/cards", cardsRouter);
