e1b95fdd9688e259fbb98e389d4627ed07069f5b
Hennadii Borysevych <genuch3@gmail.com>
2023-08-05 19:51:13 +0200

    feat:auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../../controllers");
 const { multerUpload } = require("../../middlewares");
+const { auth } = require("../../middlewares");
 
 //router.patch("/themes", usersController.updateTheme);
 
 router.patch(
   "/avatar",
+  auth,
   multerUpload.single("newAvatar"),
   usersController.uploadAvatar
 );
