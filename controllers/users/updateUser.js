cdcc046fff8555bae56e4e405c3743faa50fb386
Hennadii Borysevych <genuch3@gmail.com>
2023-08-06 23:56:27 +0200

    feat:currentUser

--- controllers/users/updateUser.js
+++ controllers/users/updateUser.js
@@ -1,23 +1,27 @@
 const bcrypt = require("bcrypt");
 
-const { cloudinary } = require("../../service")
-const { HttpError } = require("../../helpers")
-const { User }  = require("../../models")
+const { cloudinary } = require("../../service");
+const { HttpError } = require("../../helpers");
+const { User } = require("../../models");
 
 const updateUser = async (req, res, next) => {
   const { _id } = req.user;
-  const { name, email, password } = req.body;
+  const { name, email, password } = req.body.updatedUser;
   let updateFields = {};
 
-  if (name) {updateFields.name = name; }
+  if (name) {
+    updateFields.name = name;
+  }
 
-  if (email) {updateFields.email = email;}
+  if (email) {
+    updateFields.email = email;
+  }
 
   if (password) {
     const hashPassword = await bcrypt.hash(password, 10);
     updateFields.password = hashPassword;
   }
-    
+
   if (req.file && req.file.path) {
     cloudinary.uploader.upload(req.file.path, async function (err, result) {
       if (err) {
@@ -28,25 +32,23 @@ const updateUser = async (req, res, next) => {
       }
     });
   }
-updateUserData();
+  updateUserData();
   async function updateUserData() {
     try {
-      const updatedUser = await User.findByIdAndUpdate(
-        _id,
-        updateFields,
-        { new: true }
-      );
+      const updatedUser = await User.findByIdAndUpdate(_id, updateFields, {
+        new: true,
+      });
 
       if (!updatedUser) {
         return next(HttpError(404, "User not found"));
       }
 
-        res.status(200).json({ message: "Update success" });
+      res.status(200).json({ message: "Update success" });
     } catch (err) {
       next(HttpError(500, "Error updating user"));
       console.log(err.message);
     }
-    }
+  }
 };
 
- module.exports = updateUser;
+module.exports = updateUser;
