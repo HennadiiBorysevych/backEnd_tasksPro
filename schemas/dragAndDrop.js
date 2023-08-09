00f56b2a4b25165550e40297d9ff2747e84ab5bf
Volodymyr Derkach <113111513+VolodymyrDerkach97@users.noreply.github.com>
2023-08-09 11:47:48 +0300

    fix:update taskToColumnSchema

--- schemas/dragAndDrop.js
+++ schemas/dragAndDrop.js
@@ -5,17 +5,15 @@ const columnAndTask = Joi.object({
   order: Joi.number().integer().required(),
 });
 
-const taskToColumn = Joi.object({
+// Оголошення схем для масиву об'єктів
+const columnAndTaskSchema = Joi.array().items(columnAndTask);
+
+const taskToColumnSchema = Joi.object({
   idTask: Joi.string().required(),
   idColumnNew: Joi.string().required(),
   dataOld: Joi.array().items(columnAndTask).required(),
   dataNew: Joi.array().items(columnAndTask).required(),
 });
-
-// Оголошення схем для масиву об'єктів
-const columnAndTaskSchema = Joi.array().items(columnAndTask);
-
-const taskToColumnSchema = Joi.array().items(taskToColumn);
 
 module.exports = {
   columnAndTaskSchema,
