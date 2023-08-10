52f457fed409f90804e7f9979c035b4dbc6bc44e
Volodymyr Derkach <113111513+VolodymyrDerkach97@users.noreply.github.com>
2023-08-10 22:09:55 +0300

    fix:theme response rename data

--- models/user.js
+++ models/user.js
@@ -1,8 +1,6 @@
 const { Schema, model } = require("mongoose");
 const { handleMongooseError } = require("../helpers");
-const Joi = require("joi");
 
-const emailRegexp = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 const themeList = ["Light", "Violet", "Dark"];
 
 const userSchema = new Schema(
