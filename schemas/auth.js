d782b7be332c0ef24838a320a6e04d764f4f1395
Volodymyr Derkach <113111513+VolodymyrDerkach97@users.noreply.github.com>
2023-08-13 21:10:42 +0300

    fix: password  max length

--- schemas/auth.js
+++ schemas/auth.js
@@ -5,12 +5,12 @@ const emailRegexp = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 const registerSchema = Joi.object({
   name: Joi.string().min(2).required(),
   email: Joi.string().pattern(emailRegexp).required(),
-  password: Joi.string().min(6).required(),
+  password: Joi.string().min(8).required(),
 });
 
 const loginSchema = Joi.object({
   email: Joi.string().pattern(emailRegexp).required(),
-  password: Joi.string().min(6).required(),
+  password: Joi.string().min(8).required(),
 });
 
 const refreshSchema = Joi.object({
