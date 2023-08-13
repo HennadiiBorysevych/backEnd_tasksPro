714e67c5bedfd0befaf395c3570b2853a604c29d
Volodymyr Derkach <113111513+VolodymyrDerkach97@users.noreply.github.com>
2023-08-13 12:59:43 +0300

    ref: refactoring error message drag and drop

--- controllers/dragAndDrop/moveColumn.js
+++ controllers/dragAndDrop/moveColumn.js
@@ -4,28 +4,31 @@ const { HttpError } = require("../../helpers");
 const moveColumn = async (req, res) => {
   const data = req.body;
   if (!data) {
-    throw HttpError(400, "Not body");
+    throw HttpError(400, "No body");
   }
   const dataNew = [];
 
   for (const item of data) {
+    if (!item.id) {
+      throw HttpError(400, "No column id");
+    }
+
     const result = await Column.findByIdAndUpdate(
       { _id: item.id },
       { $set: { orderColumn: item.order } },
       { new: true }
     );
 
-    dataNew.push({ id: result.id, order: result.orderColumn });
-
     if (!result) {
-      throw HttpError(500, "error move column");
+      throw HttpError(500, "Error moving column in database");
     }
+    dataNew.push({ id: result.id, order: result.orderColumn });
   }
 
   res.status(200);
   res.json({
     code: 200,
-    message: "Update position column ",
+    message: "Success. Updated the column position",
     data: dataNew,
   });
 };
