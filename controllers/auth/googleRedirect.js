b1016d6f39758fa6a753011da2bfd61b96499af5
Hennadii Borysevych <genuch3@gmail.com>
2023-08-15 01:10:42 +0200

    feat:google

--- controllers/auth/googleRedirect.js
+++ controllers/auth/googleRedirect.js
@@ -27,7 +27,7 @@ const googleRedirect = async (req, res) => {
     data: {
       client_id: GOOGLE_CLIENT_ID,
       client_secret: GOOGLE_CLIENT_SECRET,
-      redirect_uri: `${BASE_URL}api/auth/google-redirect`,
+      redirect_uri: `https://backend-taskspro-public.onrender.com/api/auth/google-redirect`,
       grant_type: "authorization_code",
       code,
     },
