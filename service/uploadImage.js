7af6fa0bae6ac0b23cc159394353c05661a4488b
Pavlo-Savka <savka.pavlo@gmail.com>
2023-09-26 23:02:43 +0300

    secure true

--- service/uploadImage.js
+++ service/uploadImage.js
@@ -3,7 +3,8 @@ const cloudinary = require("cloudinary").v2
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true
 });
 
 module.exports = cloudinary;
